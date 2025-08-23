727a460ae91862c76098495afcf141886e0c9743
tatyanepgoncalves <tatygoncalves023@gmail.com>
2025-08-22 21:37:14 -0300

    feat: Created execercises with resolution about Java's fundamental

---
+++ 01-fundamentos/MeuQuartoExercicio.java
@@ -0,0 +1,14 @@
+public class MeuQuartoExercicio {
+  public static void main(String[] args) {
+    String pessoa1 = "Tatyane";
+    String pessoa2 = "Rosa";
+
+    int pessoa1Idade = 21;
+    int pessoa2Idade = 44;
+
+    int diferenca = pessoa2Idade - pessoa1Idade;
+
+    System.out.println("A diferença entre " + pessoa1 + " e " + pessoa2 + " é de " + diferenca + " anos.");
+
+  }
+}
---
+++ 01-fundamentos/MeuTerceiroExercicio.java
@@ -0,0 +1,10 @@
+public class MeuTerceiroExercicio {
+  public static void main(String[] args) {
+    double alturaRetangulo = 5;
+    double baseRetangulo = 2.5;
+    
+    double areaRetangulo = baseRetangulo * alturaRetangulo;
+
+    System.out.println("A área do retangulo é " + areaRetangulo + "m ao quadrado.");
+  }
+}

c9ef13285bbc86f9a7b52e4531d4f2817b7fba41
tatyanepgoncalves <tatygoncalves023@gmail.com>
2025-09-17 10:56:26 -0300

    remove: removendo arquivos

--- 01-fundamentos/MeuPrimeiroExercicio.java
+++
@@ -1,8 +0,0 @@
-public class MeuPrimeiroExercicio {
-  public static void main(String[] args) {
-    String name = "Tatyane";
-    int idade = 21;
-
-    System.out.println("Olá " + name + ", você tem " + idade + " anos.");
-  }
-}
--- 01-fundamentos/MeuSegundoExercicio.java
+++
@@ -1,9 +0,0 @@
-public class MeuSegundoExercicio {
-  public static void main(String[] args) {
-    double ladoQuadrado = 10;
-    double areaQuadrado = ladoQuadrado * 4;
-
-    System.out.println("A área do quadrado é: " + areaQuadrado + "m ao quadrado.");
-
-  } 
-}
